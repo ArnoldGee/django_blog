b58f664559893ffaf1f7129e15001c82bea84871
Arnau Gómez Farell <agomez1f@gmail.com>
2021-06-22 20:42:13 +0200

    added sitemap and RSS feed

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import post_list, post_detail, post_share
+from .feeds import LatestPostsFeed
 
 app_name = "blog"
 
@@ -11,4 +12,5 @@ urlpatterns = [
         "<int:year>/<int:month>/<int:day>/<slug:post>/", post_detail, name="post_detail",
     ),
     path("<int:post_id>/share/", post_share, name="post_share"),
+    path("feed/", LatestPostsFeed(), name="post_feed"),
 ]
